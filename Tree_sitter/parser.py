fdc31bb5b41630dcad122af27e3336e34c46badf
Akshar Patel <aksharmpatel1@gmail.com>
2024-05-23 17:00:16 -0500

    mid-refactoring parsing to produce dictionaries for yaml.dump

--- Tree_sitter/parser.py
+++ Tree_sitter/parser.py
@@ -1,67 +1,111 @@
 import tree_sitter_c as tsc
-from tree_sitter import Language, Parser, Node
+from tree_sitter import Language, Parser
 from typing import Generator
+from yaml import dump
 import sys
-from copy import *
 
 source = ""  # Source code string variable
 
+# # THIS CODE TRAVERSES ALL NODES (MIGHT NEED LATER SO DONT DELETE)
+# def extract_nodes(cursor) -> Generator:
+#     if not cursor.goto_first_child():
+#         return
+#     while True:
+#         # print(cursor.node.type)
+#         if cursor.node.type == "declaration" and is_function(cursor.node): yield cursor.node
+#         elif cursor.goto_first_child(): continue
+#         while not cursor.goto_next_sibling():
+#             cursor.goto_parent()
+#             if cursor.node.type == "translation_unit":
+#                 return
 
 
-def extract_nodes(cursor) -> Generator:
-    if not cursor.goto_first_child():
-        return
-    while True:
-        # print(cursor.node.type)
-        if cursor.node.type == "declaration" and is_function(cursor.node): yield cursor.node
-        elif cursor.goto_first_child(): continue
-        while not cursor.goto_next_sibling():
-            cursor.goto_parent()
-            if cursor.node.type == "translation_unit":
-                return
+def parse_translation_unit(tree) -> dict:
+    yaml = {"kind": "translation_unit"}
+    entities = []
+    for node in tree.children:
+        match node.type:
+            case "declaration":
+                entities.append(parse_decl(node))
+            case _:
+                continue
+    yaml["entities"] = entities
+    return yaml
 
-def is_function(node) -> bool:
-    declarator = node.named_child(1)
-    return declarator.type == "function_declarator"
+
+# def is_function(node) -> bool:
+#     declarator = node.named_child(1)
+#     return declarator.type == "function_declarator"
+
 
 def emit_yaml(node) -> str:
-    #initialize source code and 
+    match node,named_child(1).type:
+        case "function_declarator":
+            return emit_yaml_func(node)
+        case _:
+            return "NOT SUPPORTED"
+        
+def parse_decl(node) -> dict:
+    decl = {"kind" : "declaration"}
+    match node.named_child(1).type:
+        case "function_declarator":
+            return parse_func(node, decl)
+
+def parse_func(node, decl) -> dict:
+    # initialize source code and 
     global source
-    entry: str = "- Function:\n"
-
-    # extract and append function name
     cursor = node.walk()
-    cursor.goto_descendant(3)   # node: 'identifier' for function
+
+    # extract and record return type
+    cursor.goto_descendant(1)   # node: type: `return type`
     start = cursor.node.start_byte
     end = cursor.node.end_byte
-    entry += "\tName: \"" + source[start:end] + "\"\n"
+    decl["type"] = {"kind" : source[start:end]}
 
-    # extract and append return type
-    cursor.goto_parent()    # node: `function_declarator` for function
-    cursor.goto_previous_sibling()  # node: `primitive_type` or return type identifier for function
-    start = cursor.node.start_byte
-    end = cursor.node.end_byte
-    entry += "\tReturn_Type: \"" + source[start:end] + "\"\n"
+    # extract and record declarator
 
-    # extract and append parameters
-    entry += "\tParameters:\n"
-    cursor.goto_next_sibling()  # node: `function_declarator` for function
-    cursor.goto_descendant(3)   # node: `parameter_list` for function (under `parameters` node)
-    for node in cursor.node.named_children:
-        type_node = node.named_children[0]
-        name_node = node.named_children[1]
-        # extract and append parameter name
-        start = name_node.start_byte
-        end = name_node.end_byte
-        entry += "\t\tName: \"" + source[start:end] + "\"\n"
-        # extract and append parameter type
-        start = type_node.start_byte
-        end = type_node.end_byte
-        entry += "\t\tType: \"" + source[start:end] + "\"\n"
+    params = []
+    cursor.goto_next_sibling()  # node: `function_declarator`
+    cursor.goto
+    decl["declarators"] = [{"kind" : "declarator",
+                            "indirect_type" :
+                                {"kind" : "function",
+                                 "params": params}}]
 
 
-    # return final entry
-    return entry
+    # # extract and append function name
+    # cursor.goto_descendant(3)   # node: 'identifier' for function
+    # start = cursor.node.start_byte
+    # end = cursor.node.end_byte
+    # entry += "\tName: \"" + source[start:end] + "\"\n"
+
+    # # extract and append return type
+    # cursor.goto_parent()    # node: `function_declarator` for function
+    # cursor.goto_previous_sibling()  # node: `primitive_type` or return type identifier for function
+    # start = cursor.node.start_byte
+    # end = cursor.node.end_byte
+    # entry += "\tReturn_Type: \"" + source[start:end] + "\"\n"
+
+    # # extract and append parameters
+    # entry += "\tParameters:\n"
+    # cursor.goto_next_sibling()  # node: `function_declarator` for function
+    # cursor.goto_descendant(3)   # node: `parameter_list` for function (under `parameters` node)
+    # for node in cursor.node.named_children:
+    #     type_node = node.named_children[0]
+    #     name_node = node.named_children[1]
+    #     # extract and append parameter name
+    #     start = name_node.start_byte
+    #     end = name_node.end_byte
+    #     entry += "\t\tName: \"" + source[start:end] + "\"\n"
+    #     # extract and append parameter type
+    #     start = type_node.start_byte
+    #     end = type_node.end_byte
+    #     entry += "\t\tType: \"" + source[start:end] + "\"\n"
+
+
+    # # return final entry
+    # return entry
+
 
 
 def main():
@@ -88,12 +132,10 @@ def main():
     # Parse header file and store tree
     tree = parser.parse(bytes(source, "utf-8"))
 
-    # Initialize Cursor and run emit_yaml
-    cursor = tree.walk()
-    for node in extract_nodes(cursor): 
-        print(emit_yaml(node))
-    # pprint.pp(str(tree.root_node))
-
+    # Generate yaml and print it
+    yaml = parse_translation_unit(tree.root_node)
+    print(dump(yaml))
+    # print(str(tree.root_node))
 
 if __name__ == "__main__":
     main()
---
+++ Tree_sitter/sandbox.py
@@ -0,0 +1,3 @@
+import yaml
+
+print(yaml.dump({"a":1,"b":2}))
