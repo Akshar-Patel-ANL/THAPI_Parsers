6893f9545f7680bc644de2d61ed4e9581e172631
Akshar Patel <aksharmpatel1@gmail.com>
2024-06-07 09:57:44 -0500

    added pointer param support

--- Clang/parser.py
+++ Clang/parser.py
@@ -3,6 +3,36 @@ import yaml
 import sys
 from collections import defaultdict
 
+THAPI_types = {
+    clang.cindex.TypeKind.VOID: {"kind": "void"}, 
+    clang.cindex.TypeKind.FLOAT: {"kind": "float"},
+    clang.cindex.TypeKind.DOUBLE: {"kind": "float", "longness": 1},
+    clang.cindex.TypeKind.LONGDOUBLE: {"kind": "float", "longness": 2},
+    clang.cindex.TypeKind.INT: {"kind": "int"},
+    clang.cindex.TypeKind.UINT: {"kind": "int"},
+    clang.cindex.TypeKind.SHORT: {"kind": "int", "longness": -1},
+    clang.cindex.TypeKind.USHORT: {"kind": "int", "longness": -1, "unsigned": True},
+    clang.cindex.TypeKind.LONG: {"kind": "int", "longness": 1},
+    clang.cindex.TypeKind.ULONG: {"kind": "int", "longness": 1, "unsigned": True},
+    clang.cindex.TypeKind.LONGLONG: {"kind": "int", "longness": 2},
+    clang.cindex.TypeKind.ULONGLONG: {"kind": "int", "longness": 2, "unsigned": True},
+    clang.cindex.TypeKind.CHAR_U: {"kind": "char"},
+    clang.cindex.TypeKind.UCHAR: {"kind": "char"},
+    clang.cindex.TypeKind.CHAR_S: {"kind": "char"},
+    clang.cindex.TypeKind.SCHAR: {"kind": "char", "signed": True},
+}
+
+def to_THAPI_param(self):
+    k = self.kind
+    if k == clang.cindex.TypeKind.POINTER:
+        return {"kind": "pointer", "type": to_THAPI_param(self.get_pointee())}
+    return THAPI_types[k]
+clang.cindex.Type.to_THAPI_param = to_THAPI_param
+
+def to_THAPI_decl(self):
+    k = self.kind
+    return THAPI_types[k]
+clang.cindex.Type.to_THAPI_decl = to_THAPI_decl
 
 def parse_translation_unit(t):
     # d_entities = defaultdict(list)
@@ -40,12 +70,8 @@ def parse_type(t):
                     return {"kind": "enum", "name": d.spelling}
                 case _:
                     raise NotImplementedError(f"parse_type_ELABORATED: #{ke}")
-        case (
-            clang.cindex.TypeKind.INT
-            | clang.cindex.TypeKind.CHAR_S
-            | clang.cindex.TypeKind.DOUBLE
-        ):
-            return {"kind": str(k)}
+        case type if type in list(THAPI_types.keys()) + [clang.cindex.TypeKind.POINTER]:
+            return t.to_THAPI_param()
         case _:
             raise NotImplementedError(f"parse_type: #{k}")
 
@@ -64,16 +90,27 @@ def parse_typedef_decl(t):
 
 
 def parse_function_decl(t):
+    ret_node = t.type.get_result()
+    points = 0
+    print(ret_node.kind)
+    while ret_node.kind == clang.cindex.TypeKind.POINTER:
+        ret_node = ret_node.get_pointee()
+        points += 1
+    ptr_dict = {}
+    if points > 0:
+        ptr_dict = {"type": {"kind": "pointer"}}
+        while points > 1:
+            ptr_dict = {"type": {"kind": "points"} | ptr_dict}
     return {
         "kind": "declaration",
-        "type": {"kind": str(t.type.get_result().kind)},
+        "type": t.type.to_THAPI_decl(),
         "declarators": [
             {
                 "kind": "declarator",
-                "indirect_type": {
-                    "kind": "function",
-                    "params": [parse_parameter(a) for a in t.get_arguments()],
-                },
+                "indirect_type": 
+                    {"kind": "function"}
+                    | ptr_dict
+                    | {"params": [parse_parameter(a) for a in t.get_arguments()]},
                 "name": t.spelling,
             },
         ],
@@ -121,4 +158,5 @@ def parse_enum_decl(t):
 if __name__ == "__main__":
     t = clang.cindex.Index.create().parse(sys.argv[1]).cursor
     d = parse_translation_unit(t)
-    print(yaml.dump(d, sort_keys=False))
+    yaml.Dumper.ignore_aliases = lambda *args: True
+    print(yaml.dump(d, sort_keys=False, explicit_start=True, default_flow_style=False).strip())
---
+++ clang_tester.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+for header in ./unit_tests/*.h; do
+    echo $header
+    ruby emit_yaml.rb $header > ruby.out
+    python3 Cland/parser.py $header > ts.out
+    diff ruby.out ts.out
+done
+
