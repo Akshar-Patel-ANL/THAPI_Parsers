88abb3254d4345d98c1764fb80829598b421e54e
Akshar Patel <aksharmpatel1@gmail.com>
2024-07-08 14:43:56 -0500

    fixing github actions

--- clang_tester.sh
+++ clang_tester.sh
@@ -1,10 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
 ret=0
 for header in ./unit_tests/*.h; do
     echo $header
     ruby emit_yaml.rb $header > ruby.out
     python3 Clang/parser.py $header > ts.out
-    diff ruby.out ts.out
+    if ! diff ruby.out ts.out
+    then
     ((ret += 1))
+    fi
 done
-exit(ret)
+echo number of cases failed:
+echo $ret
