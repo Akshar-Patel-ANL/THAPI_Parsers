4b1d704bb7023b1d116cc361b7807f1a3f3252dc
Akshar Patel <aksharmpatel1@gmail.com>
2024-07-08 14:06:37 -0500

    fixing github actions

--- clang_tester.sh
+++ clang_tester.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
-
+ret=0
 for header in ./unit_tests/*.h; do
     echo $header
     ruby emit_yaml.rb $header > ruby.out
     python3 Clang/parser.py $header > ts.out
     diff ruby.out ts.out
+    ((ret += 1))
 done
-
+exit(ret)
