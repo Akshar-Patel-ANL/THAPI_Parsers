4697e5bfd8d8896eabc1e74ccd42c1e80361d400
Akshar Patel <aksharmpatel1@gmail.com>
2024-07-09 10:17:08 -0500

    added github actions

--- clang_tester.sh
+++ clang_tester.sh
@@ -1,9 +1,14 @@
-#!/bin/bash -e
-
+#!/bin/bash
+ret=0
 for header in ./unit_tests/*.h; do
     echo $header
     ruby emit_yaml.rb $header > ruby.out
     python3 Clang/parser.py $header > ts.out
-    diff ruby.out ts.out
+    if ! diff ruby.out ts.out
+    then
+    ((ret += 2))
+    fi
 done
+echo number of cases failed:
+echo $ret
 
