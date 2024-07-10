427aa38bb0747fad68154fe23cb4248952daacbe
Akshar Patel <72468876+akshar2020@users.noreply.github.com>
2024-07-10 10:55:40 -0500

    Update clang_tester.sh

--- clang_tester.sh
+++ clang_tester.sh
@@ -1,14 +1,7 @@
-#!/bin/bash
-ret=0
+#!/bin/bash -e
 for header in ./unit_tests/*.h; do
     echo $header
     ruby emit_yaml.rb $header > ruby.out
     python3 Clang/parser.py $header > ts.out
-    if ! diff ruby.out ts.out
-    then
-    ((ret += 2))
-    fi
 done
-echo number of cases failed:
-echo $ret
 
