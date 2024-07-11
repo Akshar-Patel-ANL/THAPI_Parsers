8eb79ff4e1c431b31c88e09b4e621330ed9b34cd
Akshar Patel <aksharmpatel1@gmail.com>
2024-07-11 16:17:22 -0500

    Revert "Revert "Revert "Revert "working on typedefs with structs""""
    This reverts commit 53d208254574b1f864d029e94f6509a9ea4a7bb3.

--- clang_tester.sh
+++ clang_tester.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-ret=0
+
 for header in ./unit_tests/*.h; do
     echo $header
     ruby emit_yaml.rb $header > ruby.out
     python3 Clang/parser.py $header > clang.out
-    if ! diff ruby.out clang.out
+    if ! diff ruby.out ts.out
     then
     ((ret += 1))
     fi
