6cade7d83b110a0707d9eec30bd7381d64d94401
Akshar Patel <aksharmpatel1@gmail.com>
2024-07-15 16:42:23 -0500

    indentation error

--- Clang/parser.py
+++ Clang/parser.py
@@ -77,7 +77,7 @@ def parse_translation_unit(t):
                 # Check if the struct is typedef. If yes, need to modify the dict
                 dict_typedef = next((parse_typedef_decl(c2) for c2 in t.get_children() if match_typedef_struct(c, c2)), None)
                 if dict_typedef:
-                       dict_struct = merge_typedef_struct(dict_struct, dict_typedef)
+                    dict_struct = merge_typedef_struct(dict_struct, dict_typedef)
                 entities.append(dict_struct)
             case clang.cindex.CursorKind.ENUM_DECL:
                 entities.append(parse_enum_decl(c))
